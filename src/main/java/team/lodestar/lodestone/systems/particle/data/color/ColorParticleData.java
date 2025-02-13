33d00378c0ed9bbfd23baca58939c86cc80b287c
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2025-02-13 13:16:24 +0100

    Update ColorParticleData.java

--- src/main/java/team/lodestar/lodestone/systems/particle/data/color/ColorParticleData.java
+++ src/main/java/team/lodestar/lodestone/systems/particle/data/color/ColorParticleData.java
@@ -1,5 +1,7 @@
 package team.lodestar.lodestone.systems.particle.data.color;
 
+import com.mojang.serialization.*;
+import com.mojang.serialization.codecs.*;
 import net.minecraft.util.Mth;
 import team.lodestar.lodestone.systems.easing.Easing;
 
@@ -7,11 +9,28 @@ import java.awt.*;
 
 public class ColorParticleData {
 
+    public static final Codec<ColorParticleData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Codec.FLOAT.fieldOf("r1").forGetter(data -> data.r1),
+            Codec.FLOAT.fieldOf("g1").forGetter(data -> data.g1),
+            Codec.FLOAT.fieldOf("b1").forGetter(data -> data.b1),
+            Codec.FLOAT.fieldOf("r2").forGetter(data -> data.r2),
+            Codec.FLOAT.fieldOf("g2").forGetter(data -> data.g2),
+            Codec.FLOAT.fieldOf("b2").forGetter(data -> data.b2),
+            Codec.FLOAT.fieldOf("colorCoefficient").forGetter(data -> data.colorCoefficient),
+            Easing.CODEC.fieldOf("colorCurveEasing").forGetter(data -> data.colorCurveEasing),
+            Codec.FLOAT.fieldOf("coefficientMultiplier").forGetter(data -> data.coefficientMultiplier)
+    ).apply(instance, ColorParticleData::new));
+
     public final float r1, g1, b1, r2, g2, b2;
     public final float colorCoefficient;
     public final Easing colorCurveEasing;
 
     public float coefficientMultiplier = 1;
+
+    protected ColorParticleData(float r1, float g1, float b1, float r2, float g2, float b2, float colorCoefficient, Easing colorCurveEasing, float coefficientMultiplier) {
+        this(r1, g1, b1, r2, g2, b2, colorCoefficient, colorCurveEasing);
+        this.coefficientMultiplier = coefficientMultiplier;
+    }
 
     protected ColorParticleData(float r1, float g1, float b1, float r2, float g2, float b2, float colorCoefficient, Easing colorCurveEasing) {
         this.r1 = r1;
